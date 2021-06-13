a95f0486adfe9539b95a8574e8150e365421a8ca
shamsiddin18 <shamsiddin042@mail.ru>
2021-06-13 16:25:08 +0500

    controller and router

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
-});
+});*/
+Route::redirect('/', 'users');
+Route::resource('users', UsersController::class);
