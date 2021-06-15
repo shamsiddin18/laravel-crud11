bf819b8f78f66fc43f06c1d4e076c0e8d53314a7
shamsiddin18 <shamsiddin042@mail.ru>
2021-06-15 15:33:18 +0500

    laravel-crud

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*
-Route::get('/', function () {
-    return view('welcome');
-});*/
+
 Route::redirect('/', 'users');
 Route::resource('users', UsersController::class);
